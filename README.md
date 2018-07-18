984f5c7c3612fc41339c5e8ac70c89ec0da933e8
Tejashwi Kalp Taru <tejashwi.1991@gmail.com>
2018-07-18 18:11:08 +0530

    initial work

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# tensorflow-lite-demo-android
-An android application with Tensorflow lite.
+# Tensorflow Lite Android Demo app
+
+An android application with Tensorflow lite, Mobilenet and Inception models.
+Uses the mobile camera for preview and image snapshot for image classification.
+
+
+**Dependency:**
+> implementation 'org.tensorflow:tensorflow-lite:+'
+
+The demo application uses the [camera2](https://developer.android.com/reference/android/hardware/camera2/package-summary) API for interaction with mobile camera and image capture, then the captured image is passed as a bitmap to the classifier.
+
+  
+
+###
---
+++ app/src/main/java/tejashwi/com/tensorflowsample/MainActivity.java
@@ -0,0 +1,409 @@
+package tejashwi.com.tensorflowsample;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import tejashwi.com.tensorflowsample.interfaces.Classifier;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextureView mTextureView;
+    private ImageView mPreview;
+    private TextView mResult;
+    private Button mCapture;
+
+    private final int REQUEST_CAMERA = 1;
+    private CameraDevice mCameraDevice;
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+    protected CaptureRequest.Builder mCaptureRequestBuilder;
+    protected CameraCaptureSession mCameraCaptureSessions;
+    private Size imageDimension;
+    private ImageReader imageReader;
+    private Classifier mClassifier;
+
+    private Executor executor = Executors.newSingleThreadExecutor();
+
+    /* MobileNet Quant model
+    private static final String MODEL_PATH = "mobilenet_quant_v1_224.tflite";
+    private static final String LABEL_PATH = "labels.txt";
+    */
+
+    /* Inception v3 2016 slim model */
+    private static final String MODEL_PATH = "inceptionv3_slim_2016.tflite";
+    private static final String LABEL_PATH = "imagenet_slim_labels.txt";
+
+
+    private static final int INPUT_SIZE = 224;
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(checkPermission(Manifest.permission.CAMERA, REQUEST_CAMERA)){
+            startCamera();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        closeCamera();
+        stopBackgroundThread();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                mClassifier.close();
+            }
+        });
+    }
+
+    private boolean checkPermission(String permission, int requestCode){
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, requestCode);
+                return false;
+            }
+        } else {
+            //permission is automatically granted on sdk<23 upon installation
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            if(requestCode == REQUEST_CAMERA){
+                startCamera();
+            }
+        }
+    }
+
+    private void startCamera(){
+        mTextureView = findViewById(R.id.texture_view);
+        mTextureView.setSurfaceTextureListener(textureListener);
+
+        mPreview = findViewById(R.id.preview);
+        mResult = findViewById(R.id.result);
+
+        mCapture = findViewById(R.id.capture);
+        mCapture.setOnClickListener(this);
+
+        startBackgroundThread();
+        if (mTextureView.isAvailable()) {
+            openCamera();
+        } else {
+            mTextureView.setSurfaceTextureListener(textureListener);
+        }
+
+        initTensorFlowAndLoadModel();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.capture:
+                takePicture();
+                break;
+        }
+    }
+
+    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            openCamera();
+        }
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+            // Transform you image captured size according to the surface width and height
+        }
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        }
+    };
+
+    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            mCameraDevice = camera;
+            createCameraPreview();
+        }
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            mCameraDevice.close();
+        }
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+    };
+
+    private void startBackgroundThread() {
+        mBackgroundThread = new HandlerThread("Camera Background");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    private void stopBackgroundThread() {
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void takePicture() {
+        if(null == mCameraDevice) {
+            return;
+        }
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            if(manager != null) {
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(mCameraDevice.getId());
+                StreamConfigurationMap scMap = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                if(scMap == null){
+                    Toast.makeText(this, "Configuration map is null", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Size[] jpegSizes = scMap.getOutputSizes(ImageFormat.JPEG);
+                int width = 640;
+                int height = 480;
+                if (jpegSizes != null && 0 < jpegSizes.length) {
+                    width = jpegSizes[0].getWidth();
+                    height = jpegSizes[0].getHeight();
+                }
+
+                ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+                List<Surface> outputSurfaces = new ArrayList<>(2);
+                outputSurfaces.add(reader.getSurface());
+                outputSurfaces.add(new Surface(mTextureView.getSurfaceTexture()));
+
+                final CaptureRequest.Builder captureBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+                captureBuilder.addTarget(reader.getSurface());
+                captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+                // Orientation
+                int rotation = getWindowManager().getDefaultDisplay().getRotation();
+                captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
+
+                ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
+                    @Override
+                    public void onImageAvailable(ImageReader reader) {
+                        Image image = reader.acquireLatestImage();
+                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                        byte[] bytes = new byte[buffer.capacity()];
+                        buffer.get(bytes);
+                        image.close();
+
+                        final Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        final List<Classifier.Recognition> results = mClassifier.recognizeImage(bmp);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mPreview.setImageBitmap(bmp);
+                                mResult.setText(results.toString());
+                            }
+                        });
+
+                    }
+                };
+
+                reader.setOnImageAvailableListener(readerListener, mBackgroundHandler);
+
+                final CameraCaptureSession.CaptureCallback captureListener = new CameraCaptureSession.CaptureCallback() {
+                    @Override
+                    public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request,
+                                                   @NonNull TotalCaptureResult result) {
+                        super.onCaptureCompleted(session, request, result);
+                        Toast.makeText(MainActivity.this, "Captured", Toast.LENGTH_SHORT).show();
+                        createCameraPreview();
+                    }
+                };
+
+                mCameraDevice.createCaptureSession(outputSurfaces, new CameraCaptureSession.StateCallback() {
+                    @Override
+                    public void onConfigured(@NonNull CameraCaptureSession session) {
+                        try {
+                            session.capture(captureBuilder.build(), captureListener, mBackgroundHandler);
+                        } catch (CameraAccessException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                    }
+                }, mBackgroundHandler);
+            } else {
+                Toast.makeText(MainActivity.this, "Manager is null", Toast.LENGTH_LONG).show();
+            }
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createCameraPreview() {
+        try {
+            SurfaceTexture texture = mTextureView.getSurfaceTexture();
+            texture.setDefaultBufferSize(imageDimension.getWidth(), imageDimension.getHeight());
+            Surface surface = new Surface(texture);
+            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mCaptureRequestBuilder.addTarget(surface);
+            mCameraDevice.createCaptureSession(Collections.singletonList(surface), new CameraCaptureSession.StateCallback(){
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    //The camera is already closed
+                    if (null == mCameraDevice) {
+                        return;
+                    }
+                    // When the session is ready, we start displaying the preview.
+                    mCameraCaptureSessions = cameraCaptureSession;
+                    updatePreview();
+                }
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    Toast.makeText(MainActivity.this, "Configuration change", Toast.LENGTH_SHORT).show();
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void openCamera() {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            if(manager != null) {
+                String cameraId = manager.getCameraIdList()[0];
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                if (map != null) {
+                    imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
+                    manager.openCamera(cameraId, stateCallback, null);
+                }
+            } else {
+                Toast.makeText(this, "Manager is null", Toast.LENGTH_LONG).show();
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updatePreview() {
+        if(null == mCameraDevice) {
+            return;
+        }
+        mCaptureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+        try {
+            mCameraCaptureSessions.setRepeatingRequest(mCaptureRequestBuilder.build(), null, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeCamera() {
+        if (null != mCameraDevice) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+        if (null != imageReader) {
+            imageReader.close();
+            imageReader = null;
+        }
+    }
+
+    private void initTensorFlowAndLoadModel(){
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mClassifier = TensorFlowImageClassifier.create(
+                            getAssets(),
+                            MODEL_PATH,
+                            LABEL_PATH,
+                            INPUT_SIZE);
+                    makeButtonVisible();
+                } catch (final Exception e) {
+                    throw new RuntimeException("Error initializing TensorFlow!", e);
+                }
+            }
+        });
+    }
+
+    private void makeButtonVisible(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mCapture.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
